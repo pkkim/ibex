0abcc4d16534090990f587be58169a15b6b88e30
Paul Kim <paul@silogy.io>
2024-09-19 16:32:37 -0700

    Print RTL sim log output

---
+++ run_regression.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Don't set -e because we want to move artifacts and print output whether or not the test fails
+
+cd dv/uvm/core_ibex
+
+make SIMULATOR=vcs TEST=riscv_jump_stress_test ITERATIONS=1
+RETCODE=$?
+
+SIM_LOG_PATH=$(find out/run/tests/**/rtl_sim.log)
+if [ $SIM_LOG_PATH != '' ]; then
+	echo "Contents of $SIM_LOG_PATH (or last 200 lines):"
+	tail -200 $SIM_LOG_PATH
+else
+	echo "No rtl_sim.log was produced."
+fi
+
+mv out/run/tests /tmp/artifacts
+
+exit $RETCODE
