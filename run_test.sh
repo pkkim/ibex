5812f58443bd4a825b8810f657c8bf08b8fb7370
Paul Kim <paul@silogy.io>
2025-06-05 14:09:05 -0700

    Simple test for backend server PR 665

--- run_test.sh
+++ run_test.sh
@@ -2,17 +2,8 @@
 
 set -euo pipefail
 
-TEST_NAME=$1
-
-cd dv/uvm/core_ibex
-
-RETCODE=0
-make SIMULATOR=vcs TEST=$TEST_NAME ITERATIONS=1 WAVES=1 || RETCODE=$?
-
-if [[ $RETCODE -ne 0 ]]; then
-    mv /tmp/code/dv/uvm/core_ibex/out/run/tests /tmp/artifacts
-    cat /tmp/artifacts/**/rtl_sim.log
-    vpd2vcd $(ls /tmp/artifacts/**/waves.vpd) /tmp/artifacts/waves.vcd
-fi
-
-exit $RETCODE
+for i in $(seq 1 50)
+do
+	echo $i
+	sleep 1
+done
